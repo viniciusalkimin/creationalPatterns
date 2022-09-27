f69f3a82f5076b6a6a536f103d80eed6333b987e
vinicius.alkimin <viniciusg_alkimin@hotmail.com>
2022-09-27 13:28:47 -0300

    Implementation of Thread Safe to Singleton

--- src/main/java/com/pluralsight/designpatterns/creational/singleton/DBSingleton.java
+++ src/main/java/com/pluralsight/designpatterns/creational/singleton/DBSingleton.java
@@ -1,17 +1,23 @@
-'package com.pluralsight.designpatterns.creational.singleton;
+package com.pluralsight.designpatterns.creational.singleton;
 
 public class DBSingleton {
 
-    private static DBSingleton instance = null;
+    private static volatile DBSingleton instance = null;
 
     private DBSingleton() {
+        if (instance != null) {
+            throw new RuntimeException("Use method getInstance() to create!");
+        }
     }
 
     public static DBSingleton getInstance() {
         if (instance == null) {
-            intance = new DBSingleton();
+            synchronized (DBSingleton.class) {
+                if (instance == null) {
+                    instance = new DBSingleton();
+                }
+            }
         }
         return instance;
     }
 }
-'
