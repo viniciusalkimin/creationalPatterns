78102f5c938517eb423673fd3065d5ab2bbfae7f
vinicius.alkimin <viniciusg_alkimin@hotmail.com>
2022-09-29 23:07:30 -0300

    Implementation of a Real Builder Pattern

--- src/main/java/com/pluralsight/designpatterns/creational/builder/LaunchOrderBeanDemo.java
+++ src/main/java/com/pluralsight/designpatterns/creational/builder/LaunchOrderBeanDemo.java
@@ -4,17 +4,17 @@ public class LaunchOrderBeanDemo {
     public static void main(String[] args) {
 
         //Object is immutable because of the telescoping constructor
-        LaunchOrderBean launchOrder = new LaunchOrderBean("Wheat","Lettuce","Mustard","Ham");
+     //   LaunchOrderBean launchOrder = new LaunchOrderBean("Wheat","Lettuce","Mustard","Ham");
 
-//        launchOrder.setBread("Wheat");
-//        launchOrder.setCondiments("Lettuce");
-//        launchOrder.setDressing("Mustard");
-//        launchOrder.setMeat("Ham");
+        LaunchOrderBean.Builder builder = new LaunchOrderBean.Builder();
+        builder.bread("Whaet").dressing("Mayo").condiments("Lettuce").meat("Turkey").build();
+        LaunchOrderBean launchBuilded = builder.build();
 
-        System.out.println(launchOrder.getBread());
-        System.out.println(launchOrder.getCondiments());
-        System.out.println(launchOrder.getDressing());
-        System.out.println(launchOrder.getBread());
+
+        System.out.println(launchBuilded.getBread());
+        System.out.println(launchBuilded.getCondiments());
+        System.out.println(launchBuilded.getDressing());
+        System.out.println(launchBuilded.getBread());
     }
 
 
